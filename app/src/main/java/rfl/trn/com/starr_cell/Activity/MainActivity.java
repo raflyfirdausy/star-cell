adf6a746ce6e82b761278219189cd30219c36469
Rafli Firdausy Irawan <firdausy.irawan@my.gavilan.edu>
2019-02-08 21:10:44 +0700

    Update gradle, karo tambahan folder2 tok akwoakw

--- app/src/main/java/rfl/trn/com/starr_cell/Activity/MainActivity.java
+++ app/src/main/java/rfl/trn/com/starr_cell/Activity/MainActivity.java
@@ -1,9 +1,12 @@
-package rfl.trn.com.starr_cell;
+package rfl.trn.com.starr_cell.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rfl.trn.com.starr_cell.R;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/rfl/trn/com/starr_cell/ReadMeFirst.java
@@ -0,0 +1,15 @@
+package rfl.trn.com.starr_cell;
+
+public class ReadMeFirst {
+    /*
+    * Kie kelas aja di hapus, kon project e rapih
+    * dadi di kategorikena per jenis
+    * activity neng folder activity, model neng folder model , dan seteruse
+    *
+    * setiap Activity di wei Variable Context ya sekalian diisi
+    * misal nang MainActivity.java digawe variable
+    * private Context context = MainActivity.this;
+    *
+    * Nama variable padakna bae karo sing neng XML
+    * */
+}
