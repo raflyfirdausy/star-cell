9ca678f589055ce550413086121592c0d66d118e
Rafli Firdausy Irawan <firdausy.irawan@my.gavilan.edu>
2019-02-10 11:43:27 +0700

    Tambahi class bantuan

--- app/src/main/java/rfl/trn/com/starr_cell/Activity/MainActivity.java
+++ app/src/main/java/rfl/trn/com/starr_cell/Activity/MainActivity.java
@@ -3,6 +3,10 @@ package rfl.trn.com.starr_cell.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import rfl.trn.com.starr_cell.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a;
+        DatabaseReference a = FirebaseDatabase.getInstance().getReference("aw");
     }
 }
