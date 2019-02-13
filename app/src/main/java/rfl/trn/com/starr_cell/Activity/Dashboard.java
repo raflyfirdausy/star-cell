057891fce677a8e3c73b6b8b46e9c923d0acf610
trian_damai <triannnurizkillah@gmail.com>
2019-02-14 06:08:28 +0700

    sedikit tambahan haha

--- app/src/main/java/rfl/trn/com/starr_cell/Activity/Dashboard.java
+++ app/src/main/java/rfl/trn/com/starr_cell/Activity/Dashboard.java
@@ -1,39 +1,73 @@
 package rfl.trn.com.starr_cell.Activity;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import rfl.trn.com.starr_cell.R;
 
 public class Dashboard extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private Context context;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.card_absen)
+    CardView cardAbsen;
+    @BindView(R.id.card_inventory)
+    CardView cardInventory;
+    @BindView(R.id.card_income)
+    CardView cardIncome;
+    @BindView(R.id.card_jual)
+    CardView cardJual;
+    @BindView(R.id.nav_view)
+    NavigationView navView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+        context = Dashboard.this;
+
         setSupportActionBar(toolbar);
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        navView.setNavigationItemSelectedListener(this);
+    }
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+    @OnClick(R.id.card_absen)
+    void absen(){
+
+    }
+    @OnClick(R.id.card_income)
+    void income(){
+
+    }
+    @OnClick(R.id.card_inventory)
+    void inventoy(){
+
+    }
+    @OnClick(R.id.card_jual)
+    void jual(){
+
     }
 
     @Override
@@ -48,41 +82,23 @@ public class Dashboard extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.dashboard, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        if (id == R.id.action_settings) {return true;}
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (id == R.id.nav_camera) {}
+        
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
--- app/src/main/java/rfl/trn/com/starr_cell/Activity/Login.java
+++ app/src/main/java/rfl/trn/com/starr_cell/Activity/Login.java
@@ -1,15 +1,37 @@
 package rfl.trn.com.starr_cell.Activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rfl.trn.com.starr_cell.Custom.MyEditText;
+import rfl.trn.com.starr_cell.Custom.MyTextView;
 import rfl.trn.com.starr_cell.R;
 
 public class Login extends AppCompatActivity {
+    private Context  context;
+
+    @BindView(R.id.myet_email)
+    MyEditText myetEmail;
+    @BindView(R.id.myet_password)
+    MyEditText myetPassword;
+    @BindView(R.id.btn_login)
+    MyTextView btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        context = Login.this;
+    }
+
+    @OnClick(R.id.btn_login)
+    void login(){
+        startActivity(new Intent(context,Dashboard.class));
     }
 }
