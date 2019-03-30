7f8b1bd52349be9aa574d6da2e0b57d2e85b5c47
trian <triannurizkillah@gmail.com>
2019-03-29 21:41:08 -0700

    tambah recyclerview,daftar konter

---
+++ app/src/main/java/com/rfl/trn/starr_cell/Adapter/AdapterDaftarKonter.java
@@ -0,0 +1,66 @@
+package com.rfl.trn.starr_cell.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.rfl.trn.starr_cell.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AdapterDaftarKonter extends RecyclerView.Adapter<AdapterDaftarKonter.MyViewHolder> {
+
+    private Context context;
+    private List<String> data = new ArrayList<>();
+
+    public AdapterDaftarKonter(Context context, List<String> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_daftar_konter, viewGroup, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        //populasikan datane disini
+        
+        binView( data);
+
+    }
+
+    private void binView(List<String> data) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_konter)
+        ImageView ivKonter;
+        @BindView(R.id.tv_namaKonter)
+        TextView tvNamaKonter;
+        @BindView(R.id.tv_detailKonter)
+        TextView tvDetailKonter;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+
+        }
+    }
+}
