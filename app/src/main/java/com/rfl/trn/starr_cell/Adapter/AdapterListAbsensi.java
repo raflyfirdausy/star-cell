bfdfa9d35223111abffee8fa321375bdd0b01801
trian <triannurizkillah@gmail.com>
2019-06-03 08:25:18 -0700

    absensi admin

--- app/src/main/java/com/rfl/trn/starr_cell/Adapter/AdapterListAbsensi.java
+++ app/src/main/java/com/rfl/trn/starr_cell/Adapter/AdapterListAbsensi.java
@@ -6,19 +6,24 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.rfl.trn.starr_cell.Custom.MyTextView;
+import com.rfl.trn.starr_cell.Model.AbsenModel;
 import com.rfl.trn.starr_cell.R;
+import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AdapterListAbsensi extends RecyclerView.Adapter<AdapterListAbsensi.MyViewHolder> {
-    private Context context;
-    private List<String> data ;
 
-    public AdapterListAbsensi(Context context, List<String> data){
+    private Context context;
+    private List<AbsenModel> data;
+
+    public AdapterListAbsensi(Context context, List<AbsenModel> data) {
         this.context = context;
         this.data = data;
     }
@@ -26,19 +31,18 @@ public class AdapterListAbsensi extends RecyclerView.Adapter<AdapterListAbsensi.
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_list_absensi,viewGroup,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_list_absensi, viewGroup, false);
 
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
-        final List<String> isidata = data;
 
-        myViewHolder.setDataKeView(isidata);
+
+        myViewHolder.setDataKeView(data.get(i));
 
     }
-
 
 
     @Override
@@ -46,13 +50,24 @@ public class AdapterListAbsensi extends RecyclerView.Adapter<AdapterListAbsensi.
         return data.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_karyawan)
+        ImageView ivKaryawan;
+        @BindView(R.id.tv_namaKonter)
+        MyTextView tvNamaKonter;
+        @BindView(R.id.tv_detailKaryawan)
+        MyTextView tvDetailKaryawan;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            ButterKnife.bind(this,itemView);
+            ButterKnife.bind(this, itemView);
         }
 
-        private void setDataKeView(List<String> data) {
+        private void setDataKeView(AbsenModel data) {
+            Picasso.get()
+                    .load(data.getUrlFoto())
+                    .into(ivKaryawan);
+            tvNamaKonter.setText(data.getIdKaryawan());
+            tvDetailKaryawan.setText(data.getIdKonter());
         }
     }
 }
--- app/src/main/java/com/rfl/trn/starr_cell/Fragment/Admin/AdminAbsensiFragment.java
+++ app/src/main/java/com/rfl/trn/starr_cell/Fragment/Admin/AdminAbsensiFragment.java
@@ -1,15 +1,55 @@
 package com.rfl.trn.starr_cell.Fragment.Admin;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rfl.trn.starr_cell.Adapter.AdapterListAbsensi;
+import com.rfl.trn.starr_cell.Custom.MyTextView;
+import com.rfl.trn.starr_cell.Helper.Bantuan;
+import com.rfl.trn.starr_cell.Model.AbsenModel;
 import com.rfl.trn.starr_cell.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class AdminAbsensiFragment extends Fragment {
+
+    @BindView(R.id.iv_karyawan)
+    ImageView ivKaryawan;
+    @BindView(R.id.tv_namaKonter)
+    MyTextView tvNamaKonter;
+    @BindView(R.id.tv_detailKaryawan)
+    MyTextView tvDetailKaryawan;
+    @BindView(R.id.ll_header)
+    LinearLayout llHeader;
+    @BindView(R.id.rv_absensi)
+    RecyclerView rvAbsensi;
+    Unbinder unbinder;
+    @BindView(R.id.ll_belum_ada_absen)
+    LinearLayout llBelumAdaAbsen;
+
+    private DatabaseReference databaseReference;
+    private List<AbsenModel> list = new ArrayList<>();
+    private AdapterListAbsensi adapterListAbsensi;
+
 
     public AdminAbsensiFragment() {
 
@@ -19,6 +59,50 @@ public class AdminAbsensiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_absensi, container, false);
+        View view = inflater.inflate(R.layout.fragment_admin_absensi, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        semuaAbsen();
+    }
+
+    private void semuaAbsen() {
+        databaseReference.child("karyawan")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        list.clear();
+                        if (dataSnapshot.exists()) {
+                            llBelumAdaAbsen.setVisibility(View.GONE);
+                            AbsenModel model;
+                            for (DataSnapshot data : dataSnapshot.getChildren()) {
+                                model = data.getValue(AbsenModel.class);
+                                list.add(model);
+                            }
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+                            rvAbsensi.setLayoutManager(layoutManager);
+                            adapterListAbsensi = new AdapterListAbsensi(getActivity(), list);
+                            rvAbsensi.setAdapter(adapterListAbsensi);
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        new Bantuan(getActivity()).swal_error(databaseError.getMessage());
+                    }
+                });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
