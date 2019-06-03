964aa49ee9dc101639adfc49148ab2593d31e7ab
trian <triannurizkillah@gmail.com>
2019-06-03 14:14:10 -0700

    dialog confirm absen

--- app/src/main/java/com/rfl/trn/starr_cell/Adapter/AdapterListAbsensi.java
+++ app/src/main/java/com/rfl/trn/starr_cell/Adapter/AdapterListAbsensi.java
@@ -2,14 +2,24 @@ package com.rfl.trn.starr_cell.Adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
 import com.rfl.trn.starr_cell.Custom.MyTextView;
 import com.rfl.trn.starr_cell.Model.AbsenModel;
+import com.rfl.trn.starr_cell.Model.KaryawanModel;
+import com.rfl.trn.starr_cell.Model.KonterModel;
 import com.rfl.trn.starr_cell.R;
 import com.squareup.picasso.Picasso;
 
@@ -19,6 +29,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AdapterListAbsensi extends RecyclerView.Adapter<AdapterListAbsensi.MyViewHolder> {
+
 
     private Context context;
     private List<AbsenModel> data;
@@ -57,17 +68,83 @@ public class AdapterListAbsensi extends RecyclerView.Adapter<AdapterListAbsensi.
         MyTextView tvNamaKonter;
         @BindView(R.id.tv_detailKaryawan)
         MyTextView tvDetailKaryawan;
+        @BindView(R.id.ll_parent_absen)
+        CardView llParent;
+
+        DatabaseReference databaseReference;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
 
-        private void setDataKeView(AbsenModel data) {
+        private void setDataKeView(final AbsenModel data) {
+            final SweetAlertDialog dialog = new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE);
+
+
+            databaseReference = FirebaseDatabase.getInstance().getReference();
+            databaseReference.child("karyawan")
+                    .child(data.getIdKaryawan())
+                    .addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                KaryawanModel karyawanModel = new KaryawanModel();
+                                karyawanModel = dataSnapshot.getValue(KaryawanModel.class);
+                                tvNamaKonter.setText(karyawanModel.getNamaKaryawan());
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+            databaseReference.child("konter")
+                    .child(data.getIdKonter())
+                    .addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                KonterModel konterModel = new KonterModel();
+                                konterModel = dataSnapshot.getValue(KonterModel.class);
+                                tvDetailKaryawan.setText(konterModel.getNamaKonter());
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
             Picasso.get()
                     .load(data.getUrlFoto())
                     .into(ivKaryawan);
-            tvNamaKonter.setText(data.getIdKaryawan());
-            tvDetailKaryawan.setText(data.getIdKonter());
+            llParent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.setTitleText("Konfirmasi");
+                    dialog.setContentText("Karyawan"+tvDetailKaryawan.getText()+".\n Telah melakuan absen pada "+data.getWaktuMasuk());
+                    dialog.setConfirmText("Terima");
+                    dialog.setCancelText("Tolak");
+                    dialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sweetAlertDialog) {
+
+                            dialog.dismissWithAnimation();
+                        }
+                    });
+                    dialog.setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sweetAlertDialog) {
+                            dialog.dismissWithAnimation();
+                        }
+                    });
+                    dialog.show();
+                }
+            });
+
         }
     }
 }
