f4527e70a21e03b522e6a7cb6de1d6b3a67f78ec
Rafli Firdausy Irawan <firdausy.irawan@my.gavilan.edu>
2019-06-04 14:26:16 +0700

    tambah scrollview nang loogin

--- app/src/main/java/com/rfl/trn/starr_cell/ActivityKaryawan/ListCurrentKaryawanActivity.java
+++ app/src/main/java/com/rfl/trn/starr_cell/ActivityKaryawan/ListCurrentKaryawanActivity.java
@@ -1,15 +1,36 @@
 package com.rfl.trn.starr_cell.ActivityKaryawan;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.rfl.trn.starr_cell.Custom.MyTextView;
 import com.rfl.trn.starr_cell.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ListCurrentKaryawanActivity extends AppCompatActivity {
+
+    @BindView(R.id.iv_karyawan)
+    ImageView ivKaryawan;
+    @BindView(R.id.tv_judul)
+    MyTextView tvJudul;
+    @BindView(R.id.tv_detailKaryawan)
+    MyTextView tvDetailKaryawan;
+    @BindView(R.id.ll_header)
+    LinearLayout llHeader;
+    @BindView(R.id.ll_belum_ada_absen_masuk)
+    LinearLayout llBelumAdaAbsenMasuk;
+    @BindView(R.id.rv_daftarCurrentKaryawan)
+    RecyclerView rvDaftarCurrentKaryawan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_current_karyawan);
+        ButterKnife.bind(this);
     }
 }
