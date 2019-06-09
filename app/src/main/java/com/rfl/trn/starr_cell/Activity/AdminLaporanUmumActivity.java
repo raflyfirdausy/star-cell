b43f08ef9f265ce3d6a449c6ab7ee976dd5c3154
trian <triannurizkillah@gmail.com>
2019-06-09 10:22:53 -0700

    laporan umum

--- app/src/main/java/com/rfl/trn/starr_cell/Activity/AdminLaporanUmumActivity.java
+++ app/src/main/java/com/rfl/trn/starr_cell/Activity/AdminLaporanUmumActivity.java
@@ -1,15 +1,28 @@
 package com.rfl.trn.starr_cell.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.anychart.AnyChartView;
 import com.rfl.trn.starr_cell.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.supercharge.shimmerlayout.ShimmerLayout;
+
 public class AdminLaporanUmumActivity extends AppCompatActivity {
+
+    @BindView(R.id.cv_beratBadan)
+    AnyChartView cvBeratBadan;
+    @BindView(R.id.shimmer_text)
+    ShimmerLayout shimmerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_laporan_umum);
+        ButterKnife.bind(this);
+
+        shimmerText.startShimmerAnimation();
     }
 }
